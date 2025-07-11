91da6188cba2a126f8a1d23725053ed47300e549
PeweezDev <157630150+jalinski@users.noreply.github.com>
2025-07-11 12:43:24 +0200

    Initial commit of the project structure

---
+++ GetYaTube/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GetYaTube.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ GetYaTube/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GetYaTube.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
